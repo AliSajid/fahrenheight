fedb53f9c149cd2f7d96fefce7b3effa2d7145e2
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-10-30 20:21:49 -0400

    feat: unignore the lib folder

---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: 2025 Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
+//
+// SPDX-License-Identifier: MIT
+
+// place files you want to import through the `$lib` alias in this folder.
